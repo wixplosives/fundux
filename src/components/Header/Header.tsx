f05b9b2b36b189299fd35032d14ace7c649fa78b
dorza <dorza@wix.com>
2024-04-11 15:43:34 +0300

    save comments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styles from './Header.module.css';
 
 import Logo from '../Logo';
+// import NavBar from '../NavBar';
+// import UserAuth from '../UserAuth';
 
 function Header() {
     return (
         <header className={styles.header}>
             <Logo />
+            {/* <NavBar /> */}
+            {/* <UserAuth /> */}
         </header>
     );
 }
