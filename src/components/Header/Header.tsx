17e8e6ac16017feef5737e1364e45b7cd56b813d
dorza <dorza@wix.com>
2024-04-11 18:09:29 +0300

    save comments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from './Header.module.css';
 import Logo from '../Logo';
+// import NavBar from '../NavBar';
+// import UserAuth from '../UserAuth';
 
 function Header() {
     return (
         <header className={styles.header}>
             <Logo />
+            {/* <NavBar /> */}
+            {/* <UserAuth /> */}
         </header>
     );
 }
