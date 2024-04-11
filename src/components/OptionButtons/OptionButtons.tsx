d45c7ddf72c522f5b7d8882b42f7f3b00dfe0b8d
dorza <dorza@wix.com>
2024-04-11 17:28:59 +0300

    save comments

--- src/components/OptionButtons/OptionButtons.tsx
+++ src/components/OptionButtons/OptionButtons.tsx
@@ -1,9 +1,15 @@
 import styles from './OptionButtons.module.css';
 
+// import SortByButton from '../SortByButton';
+// import FilterButton from '../FilterButton';
+// import SearchBar from '../SearchBar';
+
 function OptionButtons() {
     return (
         <div className={styles.optionButtons}>
-
+            {/* <SortByButton />
+            <FilterButton />
+            <SearchBar /> */}
         </div>
     );
 };
