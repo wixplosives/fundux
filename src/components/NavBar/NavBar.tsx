55f60688180f8d1515cddf37108e29c8b4f5525d
dorza <dorza@wix.com>
2024-04-11 13:09:55 +0300

    feat(header): add navbar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './NavBar.module.css';
+
+function NavBar() {
+    return (
+        <nav className={styles.navBar}>
+            <ul className={styles.navList}>
+                <li className={styles.navItem}>Home</li>
+                <li className={styles.navItem}>About Us</li>
+                <li className={styles.navItem}>How It Works</li>
+                <li className={styles.navItem}>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
