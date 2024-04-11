2166ca6fb645c4a148a5385893858e190c618d1e
dorza <dorza@wix.com>
2024-04-11 15:14:29 +0300

    feat: add optionbuttons to app

---
+++ src/components/OptionButtons/OptionButtons.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './OptionButtons.module.css';
+
+
+function OptionButtons() {
+    return (
+        <div className={styles.optionButtons}>
+
+        </div>
+    );
+};
+
+export default OptionButtons;
