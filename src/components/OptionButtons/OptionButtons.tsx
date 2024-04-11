3d91d6f73189e580fe8b3507bd7c4839a06e8440
dorza <dorza@wix.com>
2024-04-11 17:26:38 +0300

    chore: remove unnecessary import

--- src/components/OptionButtons/OptionButtons.tsx
+++ src/components/OptionButtons/OptionButtons.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
 import styles from './OptionButtons.module.css';
-
-// import SortByButton from '../SortByButton';
-// import FilterButton from '../FilterButton';
-// import SearchBar from '../SearchBar';
 
 function OptionButtons() {
     return (
         <div className={styles.optionButtons}>
-            {/* <SortByButton />
-            <FilterButton />
-            <SearchBar /> */}
+
         </div>
     );
 };
