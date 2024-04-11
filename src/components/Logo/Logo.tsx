78e0f1a228bfa2326aebb7c3d2ab0d9c8e8a3e98
dorza <dorza@wix.com>
2024-04-11 13:00:50 +0300

    feat(header): add logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Logo.module.css';
+
+function Logo() {
+    return (
+        <div className={styles.logo}>
+            <h1>Fundux</h1>
+        </div>
+    );
+}
+
+export default Logo;
