d7b3615d2a3c5af9742497cf37a413af807fe714
dorza <dorza@wix.com>
2024-04-11 15:16:51 +0300

    saving changes

--- src/components/OptionButtons/OptionButtons.tsx
+++ src/components/OptionButtons/OptionButtons.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './OptionButtons.module.css';
 
+// import SortByButton from '../SortByButton';
+// import FilterButton from '../FilterButton';
+// import SearchBar from '../SearchBar';
 
 function OptionButtons() {
     return (
         <div className={styles.optionButtons}>
-
+            {/* <SortByButton />
+            <FilterButton />
+            <SearchBar /> */}
         </div>
     );
 };
