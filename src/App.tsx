a766c5c65acadf9b2b2b9523da997345cbc6ff2c
dorza <dorza@wix.com>
2024-04-11 17:37:26 +0300

    chore: remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import styles from './App.module.scss';
 import Header from './components/Header';
-// import HeroSection from './components/HeroSection';
-// import OptionButtons from './components/OptionButtons';
-
 
 function App() {
     return (
-        <div className={styles.App}>
+        <div>
             <Header />
-            {/* <HeroSection />
-            <OptionButtons /> */}
         </div>
     );
 }
