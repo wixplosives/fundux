6cfc95feb3ef0d39335bffe0ee03c6f63224ff0f
dorza <dorza@wix.com>
2024-04-11 18:26:25 +0300

    chore: remove comments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,10 @@
 import styles from './Header.module.css';
 import Logo from '../Logo';
-// import NavBar from '../NavBar';
-// import UserAuth from '../UserAuth';
 
 function Header() {
     return (
         <header className={styles.header}>
             <Logo />
-            {/* <NavBar /> */}
-            {/* <UserAuth /> */}
         </header>
     );
 }
