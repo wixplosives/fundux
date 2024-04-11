4d5b0251ea8cb355683e4951699663c0450a0d69
dorza <dorza@wix.com>
2024-04-11 14:40:37 +0300

    feat(header): add userauth

---
+++ src/components/UserAuth/UserAuth.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './UserAuth.module.css';
+
+function UserAuth() {
+  return (
+    <div className={styles.userAuth}>
+      <button className={styles.authButton}>
+        Log In
+      </button>
+      <button className={styles.authButton}>
+        Sign Up
+      </button>
+    </div>
+  );
+}
+
+export default UserAuth;
