d5b5d5b1801bacc0c0e32c5f72e7238b8dda0042
dorza <dorza@wix.com>
2024-04-14 19:11:34 +0300

    style(header): switch unit system and remove unnecessary properties

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css';
 import Logo from '../Logo';
+import NavBar from '../NavBar';
 
 function Header() {
     return (
         <header className={styles.header}>
             <Logo />
+            <NavBar />
         </header>
     );
 }
