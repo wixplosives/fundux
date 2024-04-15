4f52a28f579f40bda0d7c4682209b660ff489d19
DorMosheZarka <166022754+DorMosheZarka@users.noreply.github.com>
2024-04-15 16:58:23 +0300

    feat(header): add navbar (#2)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import styles from './Header.module.css';
 import Logo from '../Logo';
+import NavBar from '../NavBar';
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <Logo />
-        </header>
-    );
+   return (
+      <header className={styles.header}>
+         <Logo />
+         <NavBar />
+      </header>
+   );
 }
 
 export default Header;
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,11 @@
 import styles from './Logo.module.css';
 
 function Logo() {
-    return (
-        <div className={styles.logo}>
-            <h1>Fundux</h1>
-        </div>
-    );
+   return (
+      <div className={styles.logo}>
+         <h1>Fundux</h1>
+      </div>
+   );
 }
 
 export default Logo;
