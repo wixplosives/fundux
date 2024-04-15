d1ed437cc69575c1e8874326a8152352d1940abd
dorza <dorza@wix.com>
2024-04-15 17:33:29 +0300

    style(header): move font url to index and add style to userauth

--- src/components/UserAuth/UserAuth.tsx
+++ src/components/UserAuth/UserAuth.tsx
@@ -1,16 +1,12 @@
 import styles from './UserAuth.module.css';
 
 function UserAuth() {
-  return (
-    <div className={styles.userAuth}>
-      <button className={styles.authButton}>
-        Log In
-      </button>
-      <button className={styles.authButton}>
-        Sign Up
-      </button>
-    </div>
-  );
+   return (
+      <div className={styles.userAuth}>
+         <button className={styles.authButton}>Log In</button>
+         <button className={styles.authButton}>Sign Up</button>
+      </div>
+   );
 }
 
 export default UserAuth;
