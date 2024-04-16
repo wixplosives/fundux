2b1a41b1bd52752a4d3c799d93fa3d12cf2dcf02
DorMosheZarka <166022754+DorMosheZarka@users.noreply.github.com>
2024-04-16 15:52:28 +0300

    feat(header): add userauth (#3)
    * feat(header): add login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import styles from './Header.module.css';
 import Logo from '../Logo';
 import NavBar from '../NavBar';
+import LogIn from '../LogIn';
 
 function Header() {
    return (
       <header className={styles.header}>
          <Logo />
          <NavBar />
+         <LogIn />
       </header>
    );
 }
