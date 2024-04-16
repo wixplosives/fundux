621e86d8880ba357167dfdba608a81c236b0fe5a
dorza <dorza@wix.com>
2024-04-16 14:21:15 +0300

    chore(header): use gap instead margin

--- src/components/UserAuth/UserAuth.tsx
+++ src/components/UserAuth/UserAuth.tsx
@@ -3,9 +3,7 @@ import styles from './UserAuth.module.css';
 function UserAuth() {
    return (
       <div className={styles.userAuth}>
-         <button className={`${styles.authButton} ${styles.first}`}>
-            Log In
-         </button>
+         <button className={styles.authButton}>Log In</button>
          <button className={styles.authButton}>Sign Up</button>
       </div>
    );
