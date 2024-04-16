4ef17865242963a55cb0633e2f6cccfae5c68c1f
dorza <dorza@wix.com>
2024-04-16 10:26:48 +0300

    style(header): change button hover bakcground color

--- src/components/UserAuth/UserAuth.tsx
+++ src/components/UserAuth/UserAuth.tsx
@@ -3,7 +3,9 @@ import styles from './UserAuth.module.css';
 function UserAuth() {
    return (
       <div className={styles.userAuth}>
-         <button className={styles.authButton}>Log In</button>
+         <button style={{ marginRight: '0.5em' }} className={styles.authButton}>
+            Log In
+         </button>
          <button className={styles.authButton}>Sign Up</button>
       </div>
    );
