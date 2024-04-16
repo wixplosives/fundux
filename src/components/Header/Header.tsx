bd7b3fabd846ba9788e5a1f4b099cf237b8dc6e4
dorza <dorza@wix.com>
2024-04-16 15:07:04 +0300

    chore(header): change components name and remove width for even gaps

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import styles from './Header.module.css';
 import Logo from '../Logo';
 import NavBar from '../NavBar';
-import UserAuth from '../UserAuth';
+import LogIn from '../LogIn';
 
 function Header() {
    return (
       <header className={styles.header}>
          <Logo />
          <NavBar />
-         <UserAuth />
+         <LogIn />
       </header>
    );
 }
