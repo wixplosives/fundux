a6237a35b687f60f745852ae4aca2f5c5839d32e
dorza <dorza@wix.com>
2024-04-16 17:12:12 +0300

    chore(herosection): change the font url position and change to more semantic element

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,14 +2,14 @@ import styles from './HeroSection.module.css';
 
 function HeroSection() {
    return (
-      <div className={styles.heroSection}>
+      <section className={styles.heroSection}>
          <h1>Transform Lives with a Single Gesture</h1>
          <h2>
             Every donation lights up a path to a brighter future. <br />
             Join us in making a real difference where it matters most. <br />
             Together, we can turn hope into action.
          </h2>
-      </div>
+      </section>
    );
 }
 
