11eb82a31b6ceed8b3a19b6c9eb066357655286d
dorza <dorza@wix.com>
2024-04-16 16:03:49 +0300

    feat(herosection): add herosection to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import HeroSection from './components/HeroSection';
 
 function App() {
    return (
       <div>
          <Header />
+         <HeroSection />
       </div>
    );
 }
