d15ca910e53ca41429042253d0f280d2d07f84e9
dorza <dorza@wix.com>
2024-04-18 11:26:37 +0300

    style(header): fix cover

--- src/components/ModalLogIn/ModalLogIn.tsx
+++ src/components/ModalLogIn/ModalLogIn.tsx
@@ -1,7 +1,11 @@
 import styles from './ModalLogIn.module.css';
 
 function ModalLogIn() {
-   return <div className={styles.modalLogIn}></div>;
+   return (
+      <div className={styles.modalLogIn}>
+         <form></form>
+      </div>
+   );
 }
 
 export default ModalLogIn;
