915c0b656e13391c9f8fcddafe92e5e5bdcc45cc
DorMosheZarka <166022754+DorMosheZarka@users.noreply.github.com>
2024-04-18 11:27:14 +0300

    feat: add optionbuttons to app (#5)
    * feat: add optionbuttons to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import OptionButtons from './components/OptionButtons';
+import SortByButton from './components/SortByButton';
 
 function App() {
    return (
       <div>
          <Header />
          <HeroSection />
+         <OptionButtons>
+            <SortByButton />
+         </OptionButtons>
       </div>
    );
 }
