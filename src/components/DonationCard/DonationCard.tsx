27fc7009d5c9640633b59d877f4014b7d7a03823
dorza <dorza@wix.com>
2024-04-18 17:43:24 +0300

    fix(donationcards): create button a seperate class

--- src/components/DonationCard/DonationCard.tsx
+++ src/components/DonationCard/DonationCard.tsx
@@ -2,47 +2,66 @@ import styles from './DonationCard.module.css';
 import React from 'react';
 
 interface DonationCardProps {
-    title: string;
-    target: number;
-    amount: number;
-    children: React.ReactNode;
-    isOpen: boolean;
-    onShowMore: () => void;
+   title: string;
+   target: number;
+   amount: number;
+   children: React.ReactNode;
+   isOpen: boolean;
+   onShowMore: () => void;
 }
 
-function DonationCard({title, target, amount, isOpen, onShowMore, children}: DonationCardProps) {
-    const progress = (amount / target) * 100;
+function DonationCard({
+   title,
+   target,
+   amount,
+   isOpen,
+   onShowMore,
+   children,
+}: DonationCardProps) {
+   const progress = (amount / target) * 100;
 
-    return (
-        <article className={styles.card} onClick={onShowMore}>
-            <header className={styles.header}>
-                <h2>{title}</h2>
-                <button className={`${progress < 33 ? styles.low : (progress < 67 ? styles.med : styles.high)}`}>
-                    Donate Now!
-                </button>
-            </header>
-            {isOpen && <div className={styles.content}>{children}</div>}
-            <footer className={styles.footer}>
-                <div className={styles.targetInfo}>
-                    <div className={styles.progressBar}>
-                        <div className={`${styles.progress} ${progress < 33 ? 
-                                           styles.low : 
-                                           progress < 67 ? styles.med : styles.high}`} 
-                             style={{
-                                    width: `${progress}%`, 
-                                    pointerEvents: "none"
-                                    }}>
-                            {progress.toFixed(0)}%
-                        </div>
-                    </div>
-                    <p className={styles.targetAmount}>${target.toLocaleString()}</p>
-                </div>
-                <button onClick={onShowMore}>
-                    {isOpen ? "Show Less" : "Show More"}
-                </button>
-            </footer>
-        </article>
-    );
-};
+   return (
+      <article className={styles.card} onClick={onShowMore}>
+         <header className={styles.header}>
+            <h2>{title}</h2>
+            <button
+               className={`${styles.btn} ${
+                  progress < 33
+                     ? styles.low
+                     : progress < 67
+                       ? styles.med
+                       : styles.high
+               }`}>
+               Donate Now!
+            </button>
+         </header>
+         {isOpen && <div className={styles.content}>{children}</div>}
+         <footer className={styles.footer}>
+            <div className={styles.targetInfo}>
+               <div className={styles.progressBar}>
+                  <div
+                     className={`${styles.progress} ${
+                        progress < 33
+                           ? styles.low
+                           : progress < 67
+                             ? styles.med
+                             : styles.high
+                     }`}
+                     style={{
+                        width: `${progress}%`,
+                        pointerEvents: 'none',
+                     }}>
+                     {progress.toFixed(0)}%
+                  </div>
+               </div>
+               <p className={styles.targetAmount}>${target.toLocaleString()}</p>
+            </div>
+            <button className={styles.btn} onClick={onShowMore}>
+               {isOpen ? 'Show Less' : 'Show More'}
+            </button>
+         </footer>
+      </article>
+   );
+}
 
 export default DonationCard;
