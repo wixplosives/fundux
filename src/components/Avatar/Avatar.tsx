16e0575aed0e8bdf2bbbc0ecc3f2016a0908dcbb
dorza <dorza@wix.com>
2024-05-05 16:03:27 +0300

    feat(avatar): add avatar to app

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,20 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from './Avatar.module.css';
 import Menu from '../Menu';
 
 function Avatar() {
    const [isOpen, setIsOpen] = useState(false);
+   const anchorRef = useRef<HTMLButtonElement>(null);
 
    const handleOnClick = () => {
       setIsOpen(!isOpen);
    };
 
+   const handleOnClose = () => {
+      setIsOpen(false);
+   };
+
+   const anchorRect = anchorRef.current
+      ? anchorRef.current.getBoundingClientRect()
+      : null;
+
    return (
       <div className={styles.avatar}>
-         <button className={styles.avatarButton} onClick={handleOnClick}>
+         <button
+            ref={anchorRef}
+            className={styles.avatarButton}
+            onClick={handleOnClick}>
             JD
          </button>
-         {isOpen && <Menu />}
+         {isOpen && <Menu onClose={handleOnClose} anchorRect={anchorRect} />}
       </div>
    );
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,44 @@
+import ReactDOM from 'react-dom';
 import styles from './Menu.module.css';
 
-function Menu() {
-   return (
-      <div className={styles.menu}>
-         <h4>John Doe</h4>
-         <ul className={styles.menuList}>
-            <li className={styles.menuItem}>Profile</li>
-            <li className={styles.menuItem}>Settings</li>
-            <li className={styles.menuItem}>Log out</li>
-         </ul>
-      </div>
+interface MenuProps {
+   anchorRect: DOMRect | null;
+   onClose: () => void;
+}
+
+function Menu({ anchorRect, onClose }: MenuProps) {
+   if (!anchorRect) return null;
+
+   const top = anchorRect.bottom + 10 + window.scrollY;
+   const right = window.innerWidth - anchorRect.right - window.scrollX;
+   const handleOnClose = (event: React.MouseEvent<HTMLDivElement>) => {
+      if (event.target === event.currentTarget) {
+         onClose();
+      }
+   };
+
+   return ReactDOM.createPortal(
+      <div className={styles.menuModal} onClick={handleOnClose}>
+         <div
+            className={styles.menu}
+            style={{
+               position: 'absolute',
+               top: `${top}px`,
+               right: `${right}px`,
+            }}>
+            <h4>John Doe</h4>
+            <ul className={styles.menuList}>
+               <li className={styles.menuItem}>Profile</li>
+               <li className={styles.menuItem}>Settings</li>
+               <li
+                  className={`${styles.menuItem} ${styles.menuLowerBoundry}`}
+                  onClick={onClose}>
+                  Log out
+               </li>
+            </ul>
+         </div>
+      </div>,
+      document.getElementById('menu-root') as HTMLElement
    );
 }
 
