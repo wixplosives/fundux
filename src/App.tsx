c4c7fb594c6ad238e51ec38f90c015d8061dd24b
dorza <dorza@wix.com>
2024-05-22 16:57:06 +0300

    chore(donation-cards): add nl and change import order

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import FiltersButton from './components/FiltersButton';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import OptionButtons from './components/OptionButtons';
 import SortByButton from './components/SortByButton';
+import FiltersButton from './components/FiltersButton';
 import DonationCards from './components/DonationCards/DonationCards';
 
 function App() {
--- src/_codux/boards/option-buttons/option-buttons.board.tsx
+++ src/_codux/boards/option-buttons/option-buttons.board.tsx
@@ -1,8 +1,19 @@
 import { createBoard } from '@wixc3/react-board';
 import OptionButtons from '../../../components/OptionButtons/OptionButtons';
+import SortByButton from '../../../components/SortByButton';
+import FiltersButton from '../../../components/FiltersButton';
 
 export default createBoard({
-    name: 'OptionButtons',
-    Board: () => <OptionButtons />,
-    isSnippet: true,
+   name: 'OptionButtons',
+   Board: () => (
+      <OptionButtons>
+         <SortByButton />
+         <FiltersButton />
+      </OptionButtons>
+   ),
+   isSnippet: true,
+   environmentProps: {
+      canvasWidth: 1078,
+      canvasHeight: 129,                    
+   },
 });
