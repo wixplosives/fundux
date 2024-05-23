1a27bc7d5c0b69043688374ff1cc4e1b4fc9eb51
dorza <dorza@wix.com>
2024-05-23 12:03:42 +0300

    chore(testing): delete junk files

--- test/e2e/example.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('example test', async ({ page }) => {
-   await page.goto('http://localhost:5173');
-   await page.getByRole('button', { name: 'Log In' }).click();
-});
