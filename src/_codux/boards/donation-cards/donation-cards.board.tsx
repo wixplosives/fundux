620d24236e5c7a4a5e7b1a099364b98f278c8a84
dorza <dorza@wix.com>
2024-05-23 14:07:43 +0300

    chore(donation-cards): change board size

--- src/_codux/boards/donation-cards/donation-cards.board.tsx
+++ src/_codux/boards/donation-cards/donation-cards.board.tsx
@@ -2,7 +2,10 @@ import { createBoard } from '@wixc3/react-board';
 import DonationCards from '../../../components/DonationCards/DonationCards';
 
 export default createBoard({
-    name: 'DonationCards',
-    Board: () => <DonationCards />,
-    isSnippet: true,
+   name: 'DonationCards',
+   Board: () => <DonationCards />,
+   isSnippet: true,
+   environmentProps: {
+      canvasWidth: 1024,
+   },
 });
