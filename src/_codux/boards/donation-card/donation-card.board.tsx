69d4da4dde63594a0b2d6e8a14e50177cd13057d
dorza <dorza@wix.com>
2024-05-26 10:36:20 +0300

    chore(donation-cards): rm duplicates

--- src/_codux/boards/donation-card/donation-card.board.tsx
+++ src/_codux/boards/donation-card/donation-card.board.tsx
@@ -1,27 +1,11 @@
 import { createBoard } from '@wixc3/react-board';
-import DonationCard from '../../../components/DonationCard/DonationCard';
+import DonationCard from '../../../components/DonationCard';
+import cards, { DonationCardData } from '../../../data/cards';
 
-interface DonationCardData {
-   id: number;
-   title: string;
-   description: string;
-   donationTarget: number;
-}
-
-const donationCard: DonationCardData = {
-   id: 1,
-   title: 'Save the Whales',
-   description: `Join our mission to save endangered whale species across the globe. 
-                Every donation contributes to oceanic conservation efforts, 
-                research initiatives to understand whale migration and health patterns, 
-                anti-poaching patrols, and educational programs aimed at raising awareness about the 
-                importance of biodiversity and marine habitats. Your generous support can make a significant difference 
-                in preserving these magnificent creatures for future generations.`,
-   donationTarget: 5000000,
-};
+const donationCard: DonationCardData = cards[0];
 
 export default createBoard({
-   name: 'DonationCard - Open',
+   name: 'DonationCard - Closed',
    Board: () => (
       <DonationCard
          key={donationCard.id}
@@ -36,6 +20,5 @@ export default createBoard({
    isSnippet: true,
    environmentProps: {
       canvasWidth: 1024,
-
    },
 });
