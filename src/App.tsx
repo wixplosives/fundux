e451fa06087866cdf10189d40c83020a92bc96df
dorza <dorza@wix.com>
2024-06-03 12:35:06 +0300

    chore(donation-cards-func):  remove old code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import React, { useState } from 'react';
 import DonationCards from './components/DonationCards';
-import DonationCardModal from './components/DonationCardModal';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import OptionButtons from './components/OptionButtons';
 import SortByButton from './components/SortByButton';
 import FiltersButton from './components/FiltersButton';
-import DonationCards from './components/DonationCards';
-
-interface DonationCard {
-   title: string;
-   target: number;
-   amount: number;
-   updated: boolean;
-}
 
 function App() {
    return (
