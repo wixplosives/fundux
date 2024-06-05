ad49a5332f7a6eba42be9cd1cc8b84553cbb15df
dorza <dorza@wix.com>
2024-06-05 16:55:54 +0300

    chore(donation-cards): change import order to align rendering order

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import DonationCards from './components/DonationCards';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import OptionButtons from './components/OptionButtons';
 import SortByButton from './components/SortByButton';
 import FiltersButton from './components/FiltersButton';
+import DonationCards from './components/DonationCards';
 
 function App() {
    return (
