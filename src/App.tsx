98eb344e8e0a54d5b92f08e8de6bfd2cbe7c87d1
dorza <dorza@wix.com>
2024-06-05 16:47:59 +0300

    chore(footer): align imports with rendering order

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Footer from './components/Footer';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import OptionButtons from './components/OptionButtons';
 import SortByButton from './components/SortByButton';
 import FiltersButton from './components/FiltersButton';
 import DonationCards from './components/DonationCards';
+import Footer from './components/Footer';
 
 function App() {
    return (
