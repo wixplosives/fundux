dc1cf0d5fb86de01e7bac3c45080efc49760c2a1
dorza <dorza@wix.com>
2024-06-06 12:34:03 +0300

    fix(avatar): fix avatar board

--- src/_codux/boards/avatar/avatar.board.tsx
+++ src/_codux/boards/avatar/avatar.board.tsx
@@ -1,8 +1,20 @@
 import { createBoard } from '@wixc3/react-board';
 import Avatar from '../../../components/Avatar/Avatar';
+import Avatar_board_module from './avatar.board.module.scss';
 
 export default createBoard({
    name: 'Avatar',
-   Board: () => <Avatar onLogOut={() => null} />,
+   Board: () => (
+      <div className={Avatar_board_module.avatarWrapper}>
+         <div id="menu-root"></div>
+         <Avatar onLogOut={() => null} />
+      </div>
+   ),
    isSnippet: true,
+   environmentProps: {
+      canvasWidth: 1024,
+      canvasHeight: 768,
+      windowWidth: 1024,
+      windowHeight: 768,
+   },
 });
