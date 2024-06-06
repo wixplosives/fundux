d41923722224d27524cbb71ab034c232c8340b96
DorMosheZarka <166022754+DorMosheZarka@users.noreply.github.com>
2024-06-06 14:38:30 +0300

    feat(router): add router to app (#12)
    * feat(router): add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import OptionButtons from './components/OptionButtons';
-import SortByButton from './components/SortByButton';
-import FiltersButton from './components/FiltersButton';
-import DonationCards from './components/DonationCards';
 
 function App() {
    return (
       <div>
          <Header />
-         <HeroSection />
-         <OptionButtons>
-            <SortByButton />
-            <FiltersButton />
-         </OptionButtons>
-         <DonationCards />
+         <Outlet />
       </div>
    );
 }
