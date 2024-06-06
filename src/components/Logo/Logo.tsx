d92414f6481cd0b262e87ab526f99885b8f3e13e
dorza <dorza@wix.com>
2024-06-06 10:36:07 +0300

    chore(playground): refactor logo style

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.css';
 function Logo() {
    return (
       <div className={styles.logo}>
-         <h1 className={styles.logoText}>Fundux</h1>
+         <h1 className={styles.text}>Fundux</h1>
       </div>
    );
 }
