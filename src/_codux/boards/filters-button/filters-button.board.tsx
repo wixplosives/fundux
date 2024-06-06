76bd0c23dc6366eed6b8b30228bd0d9b3211c9bb
dorza <dorza@wix.com>
2024-06-06 12:51:25 +0300

    chore(): add nl

--- src/_codux/boards/filters-button/filters-button.board.tsx
+++ src/_codux/boards/filters-button/filters-button.board.tsx
@@ -2,7 +2,7 @@ import { createBoard } from '@wixc3/react-board';
 import FiltersButton from '../../../components/FiltersButton/FiltersButton';
 
 export default createBoard({
-    name: 'FiltersButton',
-    Board: () => <FiltersButton />,
-    isSnippet: true,
+   name: 'FiltersButton',
+   Board: () => <FiltersButton />,
+   isSnippet: true,
 });
