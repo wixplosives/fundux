4c87a4cf363c40a1a0b62a56c5f9ae394009a423
dorza <dorza@wix.com>
2024-06-09 15:13:42 +0300

    chore(playground): remove unused import

--- src/_codux/boards/nav-bar/nav-bar.board.tsx
+++ src/_codux/boards/nav-bar/nav-bar.board.tsx
@@ -1,7 +1,6 @@
 import { createBoard } from '@wixc3/react-board';
 import NavBar from '../../../components/NavBar/NavBar';
 import { MemoryRouter } from 'react-router-dom';
-import NavBar_board_module from './nav-bar.board.module.scss';
 
 export default createBoard({
    name: 'NavBar',
