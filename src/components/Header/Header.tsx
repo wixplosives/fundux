2240ddfbbf069d982c3b6a111d8d5e68387417de
DorMosheZarka <166022754+DorMosheZarka@users.noreply.github.com>
2024-06-10 17:15:07 +0300

    feat(header): add avatar (#13)
    * feat(header): add avatar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 import Logo from '../Logo';
 import NavBar from '../NavBar';
 import LogIn from '../LogIn';
+import Avatar from '../Avatar';
 
 function Header() {
+   const [isLogedIn, setIsLogedIn] = useState(true);
+
+   const handleLogOut = () => {
+      setIsLogedIn(prev => !prev);
+   };
+
    return (
       <header className={styles.header}>
          <Logo />
          <NavBar />
-         <LogIn />
+         {isLogedIn ? <Avatar onLogOut={handleLogOut} /> : <LogIn />}
       </header>
    );
 }
