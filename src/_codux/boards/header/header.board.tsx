98ffd712feed2d038efa751d927c192ab00fac6a
dorza <dorza@wix.com>
2024-06-10 17:23:44 +0300

    fix(fix-boards): fix boards to render correcrtly

--- src/_codux/boards/header/header.board.tsx
+++ src/_codux/boards/header/header.board.tsx
@@ -1,8 +1,21 @@
 import { createBoard } from '@wixc3/react-board';
 import Header from '../../../components/Header/Header';
+import { MemoryRouter } from 'react-router-dom';
 
 export default createBoard({
-    name: 'Header',
-    Board: () => <Header />,
-    isSnippet: true,
+   name: 'Header',
+   Board: () => (
+      <MemoryRouter>
+         <Header />
+      </MemoryRouter>
+   ),
+   isSnippet: true,
+   environmentProps: {
+      canvasMargin: {
+         top: 0,
+         right: 0,
+         bottom: 0,
+         left: 0,
+      },
+   },
 });
