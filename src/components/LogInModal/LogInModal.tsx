fcc59b05939d3248aef38108e19fcf219fd3b180
dorza <dorza@wix.com>
2024-06-17 23:33:57 +0300

    chore(header-modal): remove inner divs

--- src/components/LogInModal/LogInModal.tsx
+++ src/components/LogInModal/LogInModal.tsx
@@ -16,15 +16,13 @@ function LogInModal({ onCloseModal }: LogInModalProps) {
       <div className={styles.modal} onClick={handleClick}>
          <form className={styles.modalContent}>
             <header className={`${styles.modalBoundary} ${styles.modalHeader}`}>
-               <div className={styles.modalHeaderContent}>
-                  <h2>Login to Your Account</h2>
-                  <button
-                     type="button"
-                     className={styles.xButton}
-                     onClick={() => onCloseModal()}>
-                     &times;
-                  </button>
-               </div>
+               <h2>Login to Your Account</h2>
+               <button
+                  type="button"
+                  className={styles.xButton}
+                  onClick={() => onCloseModal()}>
+                  &times;
+               </button>
             </header>
             <section className={styles.sectionContent}>
                <div className={styles.formGroup}>
@@ -53,22 +51,20 @@ function LogInModal({ onCloseModal }: LogInModalProps) {
                </button>
             </section>
             <footer className={`${styles.modalBoundary} ${styles.modalFooter}`}>
-               <div className={styles.modalFooterContent}>
+               <button
+                  type="button"
+                  className={`${styles.submitButton} ${styles.cancel}`}
+                  onClick={() => onCloseModal()}>
+                  Cancel
+               </button>
+               <p className={styles.signUp}>
+                  Don't have an account?{' '}
                   <button
-                     type="button"
-                     className={`${styles.submitButton} ${styles.cancel}`}
-                     onClick={() => onCloseModal()}>
-                     Cancel
+                     onClick={(e) => e.preventDefault()}
+                     className={styles.linkButton}>
+                     Sign up
                   </button>
-                  <p className={styles.signUp}>
-                     Don't have an account?{' '}
-                     <button
-                        onClick={(e) => e.preventDefault()}
-                        className={styles.linkButton}>
-                        Sign up
-                     </button>
-                  </p>
-               </div>
+               </p>
             </footer>
          </form>
       </div>
